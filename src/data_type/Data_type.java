1cfed36e706f802fc0f10d164ab5072c6c632b67
Vimal Raj .C <118258607+vimal33@users.noreply.github.com>
2022-12-05 21:52:01 +0530

    Add files via upload

---
+++ src/data_type/Data_type.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package data_type;
+
+/**
+ *
+ * @author Vimal
+ */
+public class Data_type {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+float value=10000000.00f;
+int value1=(int)value;
+        System.out.println("the value is:"+value1);
+        
+       
+
+
+
+    }
+    
+}
